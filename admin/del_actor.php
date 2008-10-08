09303b5f58daf4e0b2a1f75e57943e7bdaa72233
dpecos <dpecos@efbf3c10-981b-0410-b2a6-fdd4e9f049f5>
2008-10-08 18:53:37 +0000

    Initial import.

---
+++ admin/del_actor.php
@@ -0,0 +1,33 @@
+<?
+include "../language.php";
+include "../config.php";
+include "../common.php";
+
+$database=Connect();
+
+if ($HTTP_POST_VARS['id']) {
+  
+  $var=$HTTP_POST_VARS;
+  print" <center><h1>$end</h1>";
+  $result=mysql_query("SELECT name FROM ".$db_prefix."actor WHERE id=".$var['id'].";");
+  $cad=mysql_fetch_array ($result);
+  mysql_free_result($result);
+  $sql="DELETE FROM ".$db_prefix."actor WHERE id='".$var['id']."';";
+  $result=mysql_query($sql);
+  if ($result) print $correct." (".$cad[0].")\n";
+  print "<br><br><a href=\"index.php\"><img src=\"../img/back.png\" border=0></a></center>";
+ 
+} else {
+  print "<html><body>\n";
+  print "<center><h1>$d_actor</h1><form action=\"del_actor.php\" method=\"post\">\n";
+  print $name.": <select name=\"id\">\n";
+  $result=mysql_query("SELECT name,id FROM ".$db_prefix."actor ORDER BY name;");
+  while($arr = mysql_fetch_array ($result))
+    print "<option value=\"".$arr[1]."\">".$arr[0]." (".$arr[1].")\n";
+  mysql_free_result($result);
+  print "</select>\n";
+  print "<br><br><input type=\"submit\" value=\"$del\">";
+  print "</form>\n";
+  print "<br><a href=\"index.php\"><img src=\"../img/back.png\" border=0></a></center></body></html>";
+}
+?>
