8bf1df582b60411bf076a688e14883284b7a6c1b
dpecos <dpecos@efbf3c10-981b-0410-b2a6-fdd4e9f049f5>
2008-10-08 18:53:37 +0000

    Initial import.

---
+++ trunk/lang/language_de.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ *	PHPFilm
+ *
+ *	Copyright (C) 2002 Daniel Pecos Martínez
+ *
+ *	Licensed under GPL
+ */
+// SHOW DATABASE
+$id="Nr.";
+$count="Filme";
+$view="Ansicht";
+$film="Film";
+$title="Titel";
+$vo="Originale Version";
+$vo_title="Originaltitel";
+$vo_lang="Originalsprache";
+$country="Land";
+$year="Jahr";
+$length="Spieldauer";
+$productor="Vertreiber";
+$format="Format";
+$web="Web";
+$genre="Genre";
+$director="Regisseur";
+$actor="Schauspieler";
+$available="Verfügbarkeit";
+$name="Name";
+$detailed="Detailansicht";
+$simple="Einfache Ansicht";
+
+// SHOW FILM
+$t_data="DATEN";
+$t_director="REGISSEUR";
+$t_actor="SCHAUSPIELER";
+$t_arg="BEMERKUNGEN";
+
+// SHOW DIRECTOR
+$t_view_director="DATEN ZUM REGISSEUR";
+$t_director_films="FILME DES REGISSEURS";
+
+// SHOW ACTOR
+$t_view_actor="DATEN ZUM SCHAUSPIELER ";
+$t_actor_films="FILME DES SCHAUSPIELERS";
+
+// ADMIN
+$admin="Administration der Sammlung";
+$add="Hinzufügen";
+$del="Löschen";
+$edit="Bearbeiten";
+$new="Neu";
+$send="Senden";
+$change="Ändern";
+$new_dir="Neuen Regisseur eintragen";
+$new_act="Neuen Schauspieler eintragen";
+$user="Benutzer";
+$users="Benutzer";
+$list_users="Benutzer auflisten";
+$passwd_admin="Benutzer/Password Administration";
+$t_passwd="BENUTZER/PASSWORD ADMINISTRATION";
+$old_password="Altes Passwort";
+$new_password="Neues Passwort";
+$pass_err_1="Passwörter stimmen nicht überein";
+$pass_err_2="Falsches Passwort";
+$no_user="Falscher Benutzer";
+$update_suc="Passwort erfolgreich aktualisiert";
+$user_del="Benutzer erfolgreich aktualisiert";
+$user_add="Benutzer erfolgreich aktualisiert";
+$loan_admin="Verwaltung verliehener Filme";
+$t_loan="Verwaltung verliehener Filme";
+$add_person="Einen neuen Benutzer hinzufügen";
+$list_people="Zeige Benutzer";
+$people="Benutzer";
+$phone="Telefon";
+$email="Email";
+$correct="Vorgang erfolgreich beendet";
+$end="RESULTAT";
+$s_film="WÄHLE FILM";
+$n_actor="NEUER SCHAUSPIELER";
+$n_country="NEUES HERKUNFTSLAND";
+$n_director="NEUER REGISSEUR";
+$n_film="NEUER FILM";
+$n_genre="NEUES GENRE";
+$n_productor="NEUER VERTREIBER";
+$d_actor="LÖSCHE SCHAUSPIELER";
+$d_country="LÖSCHE HERKUNFTSLAND";
+$d_director="LÖSCHE REGISSEUR";
+$d_film="LÖSCHE FILM";
+$d_genre="LÖSCHE GENRE";
+$d_productor="LÖSCHE VERTREIBER";
+$e_actor="BEARBEITE SCHAUSPIELER";
+$e_country="BEARBEITE HERKUNFTSLAND";
+$e_director="BEARBEITE REGISSEUR";
+$e_film="BEARBEITE FILM";
+$e_genre="BEARBEITE GENRE";
+$e_productor="BESARBEITE VERTREIBER";
+
+$passwd_help="HILFE <br><br>\n 1- Um einen neuen Benutzer hinzuzufügen, trage alle Felder ausser $old_password ein.<br>\n 2 - Um das Passwort eines schon existierenden Benutzers zu ändern, müssen alle Felder ausgefüllt werden<br>\n 3 - Um einen Benutzer zu löschen, muss nur $user field ausgefüllt werden.\n";
+?>
