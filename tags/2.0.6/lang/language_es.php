d74e8913d0eff49a243badc5ba40985e45c9a3e6
dpecos <dpecos@efbf3c10-981b-0410-b2a6-fdd4e9f049f5>
2008-10-08 18:58:22 +0000

    Version: 2.0.6 (2004/02/22)

---
+++ tags/2.0.6/lang/language_es.php
@@ -0,0 +1,98 @@
+<?
+/*
+ *	PHPFilm
+ *
+ *	Copyright (C) 2002 Daniel Pecos Martínez
+ *
+ *	Licensed under GPL
+ */
+// SHOW DATABASE
+$count="Películas";
+$view="Ver";
+$film="Película";
+$title="Título";
+$vo="Versión Original";
+$vo_title="Título Versión Original";
+$vo_lang="Idioma Versión Original";
+$country="País";
+$year="Año";
+$length="Duración";
+$productor="Productora";
+$format="Formato";
+$web="Web";
+$genre="Género";
+$director="Director";
+$actor="Actor";
+$available="Disp.";
+$name="Nombre";
+$detailed="Vista Detallada";
+$simple="Vista Sencilla";
+
+// SHOW FILM
+$t_data="DATOS";
+$t_director="DIRECTOR";
+$t_actor="ACTORES";
+$t_arg="ARGUMENTO";
+
+// SHOW DIRECTOR
+$t_view_director="DATOS DEL DIRECTOR";
+$t_director_films="PELICULAS DEL DIRECTOR";
+
+// SHOW ACTOR
+$t_view_actor="DATOS DEL ACTOR";
+$t_actor_films="PELICULAS DEL ACTOR";
+
+// ADMIN
+$admin="Administrar Colección";
+$add="Añadir";
+$del="Borrar";
+$edit="Editar";
+$new="Nuevo";
+$send="Enviar";
+$change="Cambiar";
+$new_dir="Crear Nuevo Director";
+$new_act="Crear Nuevo Actor";
+$user="Usuario";
+$users="Usuarios";
+$list_users="Listar Usuarios";
+$passwd_admin="Administración Usuario/Password ";
+$t_passwd="ADMINISTRACION USUARIO/PASSWORD";
+$old_password="Password Viejo";
+$new_password="Password Nuevo";
+$pass_err_1="Passwords no coinciden";
+$pass_err_2="Password incorrecto";
+$no_user="Usuario incorrecto";
+$update_suc="Password actualizado correctamente";
+$user_del="Usuario borrado correctamente";
+$user_add="Usuario añadido correctamente";
+$loan_admin="Administración de Préstamos";
+$t_loan="ADMINISTRACION DE PRÉSTAMOS";
+$add_person="Añadir Nueva Persona";
+$list_people="Listar Personas";
+$people="Personas";
+$phone="Teléfono";
+$email="Email";
+$correct="Operación realizada correctamente";
+$end="RESULTADO";
+$s_film="SELECCIONAR PELÍCULA";
+$n_actor="NUEVO ACTOR";
+$n_country="NUEVA NACIONALIDAD";
+$n_director="NUEVO DIRECTOR";
+$n_film="NUEVA PELÍCULA";
+$n_genre="NUEVO GÉNERO";
+$n_productor="NUEVA PRODUCTORA";
+$d_actor="BORRAR ACTOR";
+$d_country="BORRAR NACIONALIDAD";
+$d_director="BORRAR DIRECTOR";
+$d_film="BORRAR PELÍCULA";
+$d_genre="BORRAR GÉNERO";
+$d_productor="BORRAR PRODUCTORA";
+$e_actor="EDITAR ACTOR";
+$e_country="EDITAR NACIONALIDAD";
+$e_director="EDITAR DIRECTOR";
+$e_film="EDITAR PELÍCULA";
+$e_genre="EDITAR GÉNERO";
+$e_productor="EDITAR PRODUCTORA";
+
+$passwd_help="AYUDA <br><br>\n 1 - Para añadir un nuevo usuario, debes rellenar todos los campos excepto el de $old_password.<br>\n 2 - Para cambiar el password a un usuario, rellena todos los campos.<br>\n 3 - Para borrar a un usuario, únicamente debes rellenar el campo $user.";
+?>
---
+++ tags/2.0.6/view_actor.php
@@ -0,0 +1,54 @@
+<?
+/*
+ *	PHPFilm
+ *
+ *	Copyright (C) 2002 Daniel Pecos Martínez
+ *
+ *	Licensed under GPL
+ */
+include "init.php";
+$page=page_order($page,"03");
+include "language.php";
+include "config.php";
+include "common.php";
+
+$database=Connect();
+
+$var=$HTTP_POST_VARS;
+$result=mysql_query("SELECT name, country, year FROM ".$db_prefix."actor WHERE id='".$var['actor_id']."';");
+$arr = mysql_fetch_array ($result);
+
+print "<html><head>\n";
+print "<title>".$webtitle."</title>\n";
+print "<META NAME=\"Title\" CONTENT=\"".$webtitle."\">\n";
+print "<META http-equiv=\"Content-Style-Type\" content=\"text/css\">\n";
+print "<LINK type=\"text/css\" rel=\"stylesheet\" href=\"style.css\">\n";
+print "</head>\n";
+print "<body bgcolor=\"$bgcolor\"><center>\n";
+print "<h1><font color=\"".$title_color."\">".$t_view_actor."</font></h1><br>\n";
+print "<table width=\"40%\" border=0 cellspacing=0 cellpadding=1 ><tr><td>\n";
+print "<table width=\"100%\" border=0 cellspacing=1 cellpadding=5>\n";
+print "  <tr bgcolor=\"".$bgcolor1."\">\n";
+print "    <td width=\"35%\">&nbsp;&nbsp;&nbsp;$name:</td><td><b>&nbsp;&nbsp;&nbsp;".$arr[0]."</b></td>\n";
+print "  <tr>\n";
+$tmp_result=mysql_query("SELECT name FROM ".$db_prefix."country WHERE id=".$arr[1].";");
+$arr2 = mysql_fetch_array ($tmp_result);
+mysql_free_result($tmp_result);
+print "  <tr bgcolor=\"".$bgcolor1."\">\n";
+print "    <td>&nbsp;&nbsp;&nbsp;$country:</td><td>&nbsp;&nbsp;&nbsp;".$arr2[0]."</td>\n";
+print "  <tr>\n";
+print "  <tr bgcolor=\"".$bgcolor1."\">\n";
+print "    <td>&nbsp;&nbsp;&nbsp;$year:</td><td>&nbsp;&nbsp;&nbsp;".$arr[2]."</td>\n";
+print "  <tr>\n";
+print "</table></td></tr></table><br><br>\n";
+print "<br><br>\n";
+print "<h3><font color=\"".$title_color."\">".$t_actor_films."</font></h3><br>\n";
+$query="SELECT f.* FROM ".$db_prefix."film f,".$db_prefix."actor_film af WHERE af.actor_id=".$var['actor_id']." and f.id=af.film_id ORDER BY f.title;"; 
+show_index($query);
+print "<form action=\"view_film.php\" method=\"post\">\n";
+print "<input type=\"hidden\" name=\"id\" value=\"".$var['film_id']."\">\n";
+print "<input type=\"image\" src=\"img/back.png\">\n";
+print "</form>\n";
+print "</center></body></html>\n";
+mysql_free_result($result);
+?>
